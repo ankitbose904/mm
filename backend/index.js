6c3f0113a429e0035f1a67d835b97d06a7d3a725
Ankit <ankitbose904@gmail.com>
2025-03-28 10:09:02 +0530

    v4

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:3000", "https://mm-five-murex.vercel.app/"],
+    origin: ["http://localhost:3000", "https://mm-five-murex.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true
 }));
